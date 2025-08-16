d276e55a142ef6c8529a021c866b657cef782bba
DEX0_0 <hassan13854444@gmail.com>
2025-08-16 12:00:51 +0330

    Delete price-dollar.py

--- price-dollar.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.tgju.org/profile/price_dollar_rl"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-dollar_price = soup.find('span', {'class': 'value'}).text
-
-print(f"قیمت لحظه‌ای دلار: {dollar_price} تومان")
